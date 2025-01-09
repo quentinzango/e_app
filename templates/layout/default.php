81a8ca635d47208c793efacfd9469c5a0dff7c66
quentinzango <quentinzango470@gmail.com>
2025-01-09 04:58:13 +0100

    09/01/25

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -18,6 +19,7 @@ $cakeDescription = 'ZaqMarket';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,13 +27,14 @@ $cakeDescription = 'ZaqMarket';
 
     <?= $this->Html->css('cake') ?>
     <?= $this->Html->meta('icon') ?>
-    <?= $this->Html->css(['bootstrap.min', 'fontawesome/css/all.min', 'templatemo-style']) ?>
-    
+    <?= $this->Html->css(['bootstrap.min', '/fontawesome/css/all.min', '/style/templatemo-style']) ?>
+
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
+
 <body>
     <!-- Page Loader -->
     <div id="loader-wrapper">
@@ -57,11 +60,19 @@ $cakeDescription = 'ZaqMarket';
                     <li class="nav-item"><a class="nav-link nav-link-4" href="contact.html">Contact</a></li>
                 </ul>
             </div>
+            <div class="navbar-nav ms-lg-auto flex-row justify-content-center py-3 py-lg-0 me-n2">
+                <?php if ($this->getRequest()->getSession()->read('User.loggedIn')) { ?>
+                    <?= $this->Html->link('connection', ['controller' => 'Users', 'action' => 'logout'], ['class' => 'nav-link']) ?>
+                <?php } else { ?>
+                    <?= $this->Html->link('Inscription', ['controller' => 'Users', 'action' => 'add'], ['class' => 'nav-link']) ?>
+                    <?= $this->Html->link('connection', ['controller' => 'Users', 'action' => 'login'], ['class' => 'nav-link']) ?>
+                <?php } ?>
+
+            </div>
         </div>
     </nav>
-
-    <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="img/hero.jpg">
-        <form class="d-flex tm-search-form">s
+    <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="<?= $this->Url->build('/img/hero.jpg') ?>">
+        <form class="d-flex tm-search-form">
             <input class="form-control tm-search-input" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success tm-search-btn" type="submit">
                 <i class="fas fa-search"></i>
@@ -80,7 +91,7 @@ $cakeDescription = 'ZaqMarket';
         <div class="container-fluid tm-container-small">
             <div class="row">
                 <div class="col-lg-6 col-md-12 col-12 px-5 mb-5">
-                    <h3 class="tm-text-primary mb-4 tm-footer-title">About Catalog-Z</h3>
+                    <h3 class="tm-text-primary mb-4 tm-footer-title">About ZaqMarket</h3>
                     <p>Catalog-Z is free <a rel="sponsored" href="https://v5.getbootstrap.com/">Bootstrap 5</a> Alpha 2 HTML Template for video and photo websites. You can freely use this TemplateMo layout for a front-end integration with any kind of CMS website.</p>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
@@ -121,4 +132,5 @@ $cakeDescription = 'ZaqMarket';
         });
     </script>
 </body>
+
 </html>
